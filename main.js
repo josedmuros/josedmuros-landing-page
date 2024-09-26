21d9244a28391c744ad796b41d1bb7475385f95a
Jose Daniel Murillo Ospina <josedmuros@gmail.com>
2024-09-26 06:29:50 -0500

    archivos cambiados a la raíz

---
+++ main.js
@@ -0,0 +1,40 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC55-mxUj5Nj3niXFReG44OQ&part=snippet%2Cid&order=date&maxResults=50';
+const content = null || document.getElementById('content')
+
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '12682b3b45msha9706c424abd31ap150097jsnda9a8661b7d6',
+		'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi){
+    const response = await fetch(urlApi, options);
+    const data= await response.json();
+    return data;
+}
+
+(async () =>{
+    try {
+        const videos = await fetchData(API);
+        let view = ` ${videos.items.map(video=> `
+            <div class="group relative">
+                <div
+                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                        ${video.snippet.title}
+                    </h3>
+                </div>
+            </div>
+            `).slice(0,4).join('')}            
+        `;  
+        content.innerHTML=view;      
+    } catch (error) {
+        console.log(error);
+    } 
+})();
